8f975051646fa540cf2d29f72ed2818f2d1c1981
milan <s130120@ap.be>
2026-06-25 17:33:13 +0100

    reforming this repo

---
+++ index.ts
@@ -0,0 +1,4 @@
+// packages/mmui/index.ts
+export { default as Nav } from "./src/blocks/Nav.astro";
+export { default as Checkbox } from "./src/primitives/input/Checkbox.astro";
+export { default as Button } from "./src/primitives/actions/Button.astro";
